7cff9789cb75b8e47f00e7927ab16ca159c6378c
Luisa <kisan1996zxy@gmail.com>
2019-07-11 20:02:47 +0800

    Create 1.6 String Compression.py

---
+++ 1.6 String Compression.py
@@ -0,0 +1,36 @@
+"""
+ Implement a method to perform basic string compression using the counts of repeated characters. 
+ For example, the string aabcccccaaa would become a2blc5a3. If the "compressed" string would not become smaller than the original string, 
+ your method should return the original string. You can assume the string has only uppercase and lowercase letters (a - z).
+
+"""
+
+def string_compression(string):
+    compressed = []
+    count = 1
+    i, j = 0, 1
+    while(i < j and j < len(string)):
+        if string[i] != string[j]:
+            compressed.append(string[i]+str(count))
+            count = 0
+            i = j
+        j += 1
+        count += 1
+    compressed.append(string[-1]+str(count))
+    return min(string, "".join(compressed), key = len)
+
+
+class Test(unittest.TestCase):
+    '''Test Cases'''
+    data = [
+        ('aabcccccaaa', 'a2b1c5a3'),
+        ('abcdef', 'abcdef')
+    ]
+
+    def test_string_compression(self):
+        for [test_string, expected] in self.data:
+            actual = string_compression(test_string)
+            self.assertEqual(actual, expected)
+
+if __name__ == "__main__":
+    unittest.main()
